beb5a724bc8e5a06e2c9b82d7da0ac3003d4fb41
Gochi <germannparisi@gmail.com>
2015-11-18 13:35:29 -0300

    Agrego arraylist para los métodos HTTP en la clase Parser.

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -2,12 +2,21 @@ package parser;
 
 import comun.Codigos;
 import comun.SolicitudHTTP;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
  * Parsea la solicitud HTTP
  */
 public class Parser {
+
+    static final ArrayList<String> metodosHTTP = new ArrayList<>(3);
+
+    static {
+        metodosHTTP.add("GET");
+        metodosHTTP.add("HEAD");
+        metodosHTTP.add("POST");
+    }
 
     /**
      * Parsea la cabecera HTTP.
@@ -16,7 +25,7 @@ public class Parser {
      * @return la solicitud HTTP.
      * @throws comun.Codigos.HTTPException
      */
-    public SolicitudHTTP parsearSolicitud(String cabecera) throws Codigos.HTTPException{
+    public SolicitudHTTP parsearSolicitud(String cabecera) throws Codigos.HTTPException {
         Scanner sc = new Scanner(cabecera);
         String metodo = sc.next();
         if (!esMetodoValido(metodo)) {
@@ -36,13 +45,14 @@ public class Parser {
         return solicitud;
     }
 
+    /**
+     * Este método indica si el método HTTP pasado como parámetro es válido o
+     * no. El término válido significa si es soportado por este servidor o no.
+     *
+     * @param metodo
+     * @return true o false indicando si el método es válido o no.
+     */
     public boolean esMetodoValido(String metodo) {
-        boolean ok = false;
-        switch (metodo) {
-            case "GET":
-            case "POST":
-                ok = true;
-        }
-        return ok;
+        return metodosHTTP.contains(metodo);
     }
 }
